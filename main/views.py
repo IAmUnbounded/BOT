ebde4488d42ca985cca96b8643d95db6596514a4
Shivam Kohli <shivamkohli@Shivams-MacBook-Pro.local>
2016-10-13 09:51:54 +0530

    small changes

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*- 
 
 
 from django.shortcuts import render
@@ -19,6 +19,22 @@ PAGE_ACCESS_TOKEN = 'EAAPZCiQfbWcEBAM3c4BzZBZC4ptcexlskgXTUfWj3AjZBjifr4KuuwE3Oo
 
 
 
+
+def name_generator(fbid):
+    url = 'https://graph.facebook.com/v2.6/fbid?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=' + PAGE_ACCESS_TOKEN 
+    resp = requests.get(url)
+    data =json.loads(resp.text)
+    return data
+
+
+
+
+
+
+
+
+
+
 def post_facebook_message(fbid,message_text):
 	post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
 	response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":message_text}})
@@ -26,33 +42,54 @@ def post_facebook_message(fbid,message_text):
 	print status.json()
 
 
+
+
+
+
+
+def logg(message,symbol='-'):
+    print '%s\n %s \n%s'%(symbol*10,message,symbol*10)
+
+
+
+
+
 class MyChatBotView(generic.View):
-	def get(self, request, *args, **kwargs):
-		if self.request.GET['hub.verify_token'] == VERIFY_TOKEN:
-			return HttpResponse(self.request.GET['hub.challenge'])
-		else:
-			return HttpResponse('Oops invalid token')
+    def get (self, request, *args, **kwargs):
+        if self.request.GET['hub.verify_token'] == VERIFY_TOKEN:
+            return HttpResponse(self.request.GET['hub.challenge'])
+        else:
+            return HttpResponse('Oops invalid token')
 
-	@method_decorator(csrf_exempt)
-	def dispatch(self, request, *args, **kwargs):
-		return generic.View.dispatch(self, request, *args, **kwargs)
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return generic.View.dispatch(self, request, *args, **kwargs)
 
-	def post(self, request, *args, **kwargs):
-		incoming_message= json.loads(self.request.body.decode('utf-8'))
-		print incoming_message
+    def post(self, request, *args, **kwargs):
+        incoming_message= json.loads(self.request.body.decode('utf-8'))
+        
+        logg(incoming_message)
 
-		for entry in incoming_message['entry']:
-			for message in entry['messaging']:
-				print message
-				try:
-					sender_id = message['sender']['id']
-					message_text = message['message']['text']
-					post_facebook_message(sender_id,message_text) 
-				except Exception as e:
-					print e
-					pass
+        for entry in incoming_message['entry']:
+            for message in entry['messaging']:
 
-		return HttpResponse()  
+                try:
+                    sender_id = message['sender']['id']
+                    message_text = message['message']['text']
+
+                    data = userdeatils(sender_id)
+                    name = '%s %s'%(data['first_name'],data['last_name'])
+
+                    if message_text.lower() in 'hi,hello,hey'.split(','):
+                        post_facebook_message(fbid,'hey' + name)
+
+
+                    else:
+                        post_facebook_message(sender_id,'please say hi hello hey to start')
+                            
+
+
+        return HttpResponse() 
 
 def index(request):
 	return HttpResponse('Hello world')
